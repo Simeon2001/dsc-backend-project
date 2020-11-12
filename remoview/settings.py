acf98fb6b751f6248efe44cfcbd26de0122230b4
Simeon2001 <jesusanyasimeon@gmail.com>
2020-11-12 12:32:06 +0100

    broken

--- remoview/settings.py
+++ remoview/settings.py
@@ -120,5 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'movie/static'),
+)
 if os.getcwd()=='/app':
     DEBUG=False
